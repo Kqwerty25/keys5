ac46651e8a6b371c8d89829323e8caf2ed797b36
Kqwerty25 <did.dikopolov@mail.ru>
2025-04-14 11:14:08 +0300

    version 1.0

---
+++ WebApplication3/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Управление общими сведениями о сборке осуществляется с помощью 
+' набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+' связанных с этой сборкой.
+
+' Проверка значений атрибутов сборки
+<Assembly: AssemblyTitle("WebApplication3")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication3")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+<Assembly: Guid("0e68fd58-4bab-4924-a5f8-be4b2f996ec8")>
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
